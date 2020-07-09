ed2a4f1a11bc5ec4c69e64d500857e7cd819492c
chuangfeng <947924200@qq.com>
2020-07-09 09:26:23 +0800

    修改sql模板

--- sql/haos_adminorg_delete.sql
+++ sql/haos_adminorg_delete.sql
@@ -1,3 +1,6 @@
 DELETE FROM T_HAOS_ADMINORG WHERE FID >= '#adminorgid1' and FID <= '#adminorgid2';
 DELETE FROM T_HAOS_ADMINORG_L WHERE FID >= '#adminorgid1' and FID <= '#adminorgid2';
 DELETE FROM T_HAOS_ADMINSTRUCT WHERE FID >= '#adminorgid1' and FID <= '#adminorgid2';
+DELETE FROM t_hbss_empstrategy WHERE FID>= '#adminorgid1' and FID <= '#adminorgid2';
+DELETE FROM t_hbss_empstrentry WHERE FID>= '#adminorgid1' and FID <= '#adminorgid2';
+DELETE FROM t_haos_defstratgy WHERE FID>= '#adminorgid1' and FID <= '#adminorgid2';
